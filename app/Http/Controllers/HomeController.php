0a879db966b4de8b74c617de234d7a9211be7dcf
hiramatsua <akihisa_h@icloud.com>
2021-05-04 18:24:09 +0900

    投稿一覧

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,26 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;    //Authクラスのインポート
 
 class HomeController extends Controller
 {
-    /**
-     * Create a new controller instance.
-     *
-     * @return void
-     */
-    public function __construct()
-    {
-        $this->middleware('auth');
-    }
-
-    /**
-     * Show the application dashboard.
-     *
-     * @return \Illuminate\Contracts\Support\Renderable
-     */
     public function index()
     {
+        $user = Auth::user();   // ログインユーザーを取得する
+        if (is_null($user)) {
+            abort(403);
+        }
         return view('home');
     }
 }
---
+++ resources/views/layout.blade.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>bbs-small</title>
+    @yield('styles')
+    <link rel="stylesheet" href="/css/styles.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+</head>
+<body>
+    <header>
+        <nav class="my-navbar">
+            <a class="my-navbar-brand" href="{{ route('home') }}">
+                <i class="fa fa-btn fa-home"></i> 掲示板ホーム</a>
+            <div class="my-navbar-control">
+                @if(Auth::check())
+                <!-- ログイン時 -->
+                    <span class="my-navbar-item">ようこそ, {{ Auth::user()->name }}さん</span>
+                    <form method="post" name="logout" action="{{ route('logout') }}">
+                        @csrf
+                        <a href="javascript:logout.submit()">
+                            <i class="fa fa-btn fa-sign-out"></i> ログアウト</a>
+                    </form>
+                @else
+                <!-- ログインしていない時 -->
+                    <a href="{{ route('login') }}" class="my-navbar-item">
+                        <i class="fa fa-btn fa-sign-in"></i> ログイン</a>｜
+                    <a href="{{ route('register') }}" class="my-navbar-item">
+                        <i class="fa fa-btn fa-user"></i> ユーザ登録</a>
+                @endif
+            </div>
+        </nav>
+    </header>
+    <main>
+        @yield('content')
+    </main>
+
+    @yield('scripts')
+</body>
+</html>
