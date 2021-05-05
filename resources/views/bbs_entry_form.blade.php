f737fef59ae1b59fa7e1c8cac84094cf6ac333ef
hiramatsua <akihisa_h@icloud.com>
2021-05-05 11:46:50 +0900

    投稿登録

--- resources/views/bbs_entry_form.blade.php
+++ resources/views/bbs_entry_form.blade.php
@@ -8,7 +8,7 @@
                 <nav class="panel panel-default">
                     <div class="panel-heading">投稿する</div>
                     <div class="panel-body">
-                    <!-- エラー表示 -->
+                        <!-- エラー表示 -->
                         @if($errors->any())
                             <div class="alert alert-danger">
                                 @foreach($errors->all() as $message)
@@ -18,22 +18,25 @@
                         @endif
                         <form action="{{ route('create') }}" method="POST">
                             @csrf
+                            <input type="hidden" name="user_id" value="{{ Auth::user()->id }}">
+                            <input type="hidden" name="author" value="{{ Auth::user()->name }}">
                             <div class="form-group">
                                 <label for="title">タイトル</label>
                                     <input type="text" class="form-control" name="title" id="title" value="{{ old('title') }}" />
                             </div>
                             <div class="form-group">
                                 <label for="body">本文</label>
-                                <textarea rows="4" cols="60" class="form-control" name="body" id="body" value="{{ old('body') }}">記入してください。</textarea>
+                                <textarea rows="4" cols="50" class="form-control" name="body" id="body" value="{{ old('body') }}"></textarea>
                             </div>
+
                             <div class="text-right">
                                 <button type="submit" class="btn btn-primary">投稿する</button>
-                                <p><a href="#" onClick="history.back(); return false;">
-                                        <i class="fa fa-btn fa-repeat"></i> 前のページにもどる</a></p>
                             </div>
                         </form>
                     </div>
                 </nav>
+                <p><a href="{{ route('home') }}">
+                                        <i class="fa fa-btn fa-repeat"></i> 投稿一覧にもどる</a></p>
             </div>
         </div>
     </div>
